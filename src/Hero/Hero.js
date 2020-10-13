a20ecb20654df3cc076867fe3509b8da5d3269d3
WesleyWotring <wwotring@uncc.edu>
2020-10-13 14:09:28 -0400

    Brought PBudget code in

---
+++ src/Hero/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Hero() {
+  return (
+    <div className="hero">
+        <h1>Personal Budget Management Application</h1>
+        <h2>FREE</h2>
+    </div>
+  );
+}
+
+export default Hero;
